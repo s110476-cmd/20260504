a1637feb1e5ccbbb4d2ff8e0d8265d9013fb5fd8
s110476-cmd <s110476@htsh.ntpc.edu.tw>
2026-05-04 12:18:07 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,7 @@
-let capture;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // 擷取攝影機影像
-  capture = createCapture(VIDEO);
-  // 隱藏原始的 DOM 影片元件，只在畫布上繪製
-  capture.hide();
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background('#e7c6ff');
-
-  let w = width * 0.5;
-  let h = height * 0.5;
-  let x = (width - w) / 2;
-  let y = (height - h) / 2;
-
-  push();
-  // 移動座標原點至影像區域的右側並水平翻轉，達成左右顛倒效果
-  translate(x + w, y);
-  scale(-1, 1);
-  image(capture, 0, 0, w, h);
-  pop();
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  background(220);
 }
